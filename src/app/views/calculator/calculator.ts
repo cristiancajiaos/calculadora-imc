753dc3d305c6656f30f1c1074e68c240de649fe6
Cristian Cajiao <cristiancajiaos@outlook.com>
2026-07-21 16:54:52 -0400

    Definida apariencia de vista Calculator, sin calcular el IMC

--- src/app/views/calculator/calculator.ts
+++ src/app/views/calculator/calculator.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [MatCardModule, MatSliderModule, FormsModule, MatButtonModule],
   templateUrl: './calculator.html',
   styleUrl: './calculator.scss',
 })
-export class Calculator {}
+export class Calculator {
+
+  maxHeight = signal(250);
+  minHeight = signal(100);
+  defaultHeight = signal(170);
+
+  maxWeight = signal(200);
+  minWeight = signal(0);
+  defaultWeight = signal(80);
+
+  showTickMarks = signal(true);
+}
